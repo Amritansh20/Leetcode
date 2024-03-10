d0f7cfd4dc659c2d515c79824d13510aef88b061
Amritansh20 <soni.amritansh22@gmail.com>
2024-03-10 12:02:53 +0530

    Maximize Happiness

---
+++ 3075-Maximize_Happiness.java
@@ -0,0 +1,17 @@
+class Solution {
+    public long maximumHappinessSum(int[] happiness, int k) {
+         Arrays.sort(happiness);
+        long ans =0;
+        int j = 0; 
+        int l=happiness.length -1;
+        
+        while(k-- >0){
+            if(happiness[l] - j >0){
+            ans = ans+happiness[l]-j;
+            j++;
+            l--;   
+            }
+        }
+        return ans;
+    }
+}
