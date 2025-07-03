28af66a14b81de4b6da99e05fa7e1fdc4dc53e63
Amritansh Soni <amritansh@Amritanshs-MacBook-Air.local>
2025-07-03 16:23:52 +0530

    Leetcode 2929 Number of ways to distribute candies 2

---
+++ Dynamic Programming/2929-Number_Of_ways_to_distribute_candies_2.java
@@ -0,0 +1,40 @@
+/*
+    State: 
+    dp[i][j] = Number of ways to distribute j candies till ith children.
+    
+    Transition:
+    How many candies can I allocate to a child?
+    It can be from 0 to limit. Math.min(limit,j)
+    
+    If I am giving 1 candy to a child and the limit is 3 then 2 candies will be distributed 
+    among other children
+
+    dp[i][j] = dp[i-1][j-0] + dp[i-1][j-1]+ ... dp[i-1][j-Math.min(j,limit)]
+
+    Base case:
+    I have 0 candy and 0 children then 1 wasy to distribute.
+
+    Final Subproblem:
+    dp[3][n];
+
+    This will give TLE as we are calculating the sum in thirst loop for transition.
+
+    Let's try to removed that and apply prefix sum. 
+
+ */
+class Solution {
+    public long distributeCandies(int n, int limit){
+        int[][] dp = new int[4][n+1];
+        dp[0][0]=1;
+
+        for(int i=1;i<4;i++){
+            for(int j=0;j<=n;j++){
+                dp[i][j]=0;
+                for(int k=0;k<=Math.min(j,limit);k++){
+                    dp[i][j]+= dp[i-1][j-k];
+                }
+            }
+        }
+        return dp[3][n];
+    }
+}
