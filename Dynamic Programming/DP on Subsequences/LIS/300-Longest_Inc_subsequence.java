b2dec9d39c29e98bca842f30f9124213db447fd7
Amritansh Soni <amritansh@Amritanshs-MacBook-Air.local>
2025-07-09 20:16:42 +0530

    Leetcode 300 Longest Inc Subsequence

--- Dynamic Programming/DP on Subsequences/LIS/300-Longest_Inc_subsequence.java
+++ Dynamic Programming/DP on Subsequences/LIS/300-Longest_Inc_subsequence.java
@@ -15,7 +15,7 @@
     S.C - O(n)
 
  */
-import java.util.Arrays;
+import java.util.*;
 class Solution {
     public int lengthOfLIS(int[] nums) {
         int n = nums.length;
@@ -32,5 +32,60 @@ class Solution {
             maxLen = Math.max(maxLen,dp[i]);
         }
         return maxLen;
+    }
+}
+
+/*
+    Idea - 
+    10,9,2,5,3,7,101,18
+
+    list1 = {10,101}
+    list2 ={9,101}
+    list3= {2,5,7,101} // ans
+    list4 = {3,7,101}
+    list5 ={18}
+
+    What are we doing? 
+    We are checking the the array element with the last element of list, if it is 
+    greater we add pushing it. if not the creating a new list starting with current element.
+
+    We element is greater than last element of multiple lists , we add in all. 
+
+    We cannot create so many lists so took advantage of the place where the incoming number would lie.
+
+    T.C - O(nlogn)
+    S.C - O(n)
+ */
+class Solutions {
+    public int lowerBound(List<Integer> list, int element){
+        int low=0;
+        int high = list.size()-1;
+
+        while(low<=high){
+            int mid = low + (high-low)/2;
+
+            if(element>list.get(mid))
+            low=mid+1;
+            else 
+            high = mid-1;
+        }
+        return low;
+    }
+    public int lengthOfLIS(int[] nums) {
+        int n = nums.length;
+        List<Integer> list = new ArrayList<>();
+        list.add(nums[0]);
+        int len=1;
+
+        for(int i=1;i<n;i++){
+            if(nums[i]>list.get(list.size()-1)){
+                list.add(nums[i]);
+                len++;
+            }else{
+                int index = lowerBound(list,nums[i]);
+                list.set(index,nums[i]);
+            }
+        }
+        return len;
     }
 }
