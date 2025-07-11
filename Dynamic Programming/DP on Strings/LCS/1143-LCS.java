2c8495d18aec4a0e57b7af162dc722a76aa0b9c8
Amritansh Soni <amritansh@Amritanshs-MacBook-Air.local>
2025-07-11 10:09:41 +0530

    Leetcode 1143 Longest Common Subsequence

--- Dynamic Programming/DP on Strings/LCS/1143-LCS.java
+++ Dynamic Programming/DP on Strings/LCS/1143-LCS.java
@@ -1,23 +1,45 @@
-import java.util.*;
+/*
+    Base problem for multiple other problems.
+
+    State: 
+        dp[i][j] = length of LCS of first i characters of text1 and first j characters of text2.
+    
+    Transition:
+        In case of char matching:
+        We can include this character in our subsequence, so we add 1 to the LCS length of the 
+        previous prefixes (i-1, j-1).
+        
+        In case of chars do not match:
+        In this case we cannot include both the chars i.e text1[i] and text[j].
+        We have to choose only one. But which one? The one which will give me max
+        So, we ignore text1[i] (dp[i][j]=dp[i-1][j]) text1[0...i-1] text2[0...j]
+        and then we ignore text2[j] (dp[i][j]=dp[i][j-1]) text1[0...i] text2[0....j-1]
+        and we choose the max from it.
+
+        There is the index shift dp[i][j] is not same as text1[i] and text2[j]
+        dp[i][j]=text1[i-1] text2[j-1]
+
+    Final SubProblem:
+        LCS till the last char of text1 and last char of text2. 
+        dp[m][n]
+
+ */
 class Solution {
-    static int dp[][] = new int[1001][1001];
-    public static int longSub(String text1, String text2, int m, int n){
-        if(n==0||m==0)
-        return 0;
-
-        if(dp[m][n]!=-1)
-        return dp[m][n];
-
-        if(text1.charAt(m-1) == text2.charAt(n-1))
-        return dp[m][n]=1+longSub(text1, text2, m-1,n-1);
-        else
-        return dp[m][n]=Math.max(longSub(text1,text2,m-1,n),longSub(text1,text2,m,n-1));
-    }
     public int longestCommonSubsequence(String text1, String text2) {
-        for(int row[] : dp){
-            Arrays.fill(row,-1);
+        int m = text1.length();
+        int n = text2.length();
+        int[][] dp = new int[m+1][n+1];
+        for(int i=1;i<=m;i++){
+            for(int j=1;j<=n;j++){
+                if(text1.charAt(i-1)==text2.charAt(j-1))
+                dp[i][j] = 1+dp[i-1][j-1];
+                else
+                dp[i][j] = Math.max(dp[i-1][j],dp[i][j-1]);
+
+                // dp[i-1][j] = ignoring text1[i-1]
+                // dp[i][j-1] = ignoring text2[j-1]
+            }
         }
-        longSub(text1,text2, text1.length(),text2.length());
-        return dp[text1.length()][text2.length()];
+        return dp[m][n];
     }
 }
