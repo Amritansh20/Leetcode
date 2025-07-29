03b584c5343b0e8f1e73c35b9259abb36815ddeb
Amritansh Soni <amritansh@Amritanshs-MacBook-Air.local>
2025-07-29 21:10:56 +0530

    Leetcode 187 Repeted DNA Sequence

---
+++ Sliding Window/187-Repeted_DNA_Sequence.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+/*
+    T.C - O(n)
+    S.c- O(n)
+ */
+class Solution {
+    public List<String> findRepeatedDnaSequences(String s) {
+        Map<String,Integer> map = new HashMap<>();
+        int right=0;
+        StringBuilder str = new StringBuilder();
+        while(right<s.length()){
+            str.append(s.charAt(right));
+
+            if(str.length()==10){
+                map.put(str.toString(),map.getOrDefault(str.toString(),0)+1);
+                str.deleteCharAt(0);
+            }
+            right++;
+        }
+
+        List<String> ans = new ArrayList<>();
+        for(Map.Entry<String,Integer> it : map.entrySet()){
+            if(it.getValue()>1){
+                ans.add(it.getKey());
+            }
+        }
+        return ans;
+    }
+}
